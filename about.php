a1bfde9cf736aa59d859463e786179b65a41faea
Joshua Zeitlinger <joshuaspring9@gmail.com>
2015-07-20 17:32:18 -0400

    Add team bios

--- about.php
+++ about.php
@@ -19,11 +19,20 @@
 				<div class="container">
 					<header class="major special">
 						<h2>Who are we?</h2>
-						<p>Bizni was created by a group of interns at the <a target="_blank" href="http://grandcentraltech.com/">Grand Central Tech Innovation</a> project as a response to the challenge of the Business to Business (B2B) payment problem.  Communication and responsiveness is key; without these, invoices often go unpaid and relationships become strained.  Bizni aims to eliminate these problems through an all in one style platform where businesses small and large can come together in an open, honest, and easy environment.</p>
+						<p>Some information about the people behind Bizni</p>
 					</header>
+					<h3>Background</h3>
+					<p>Bizni was created by a group of interns at the <a target="_blank" href="http://grandcentraltech.com/">Grand Central Tech Innovation</a> project as a response to the challenge of the Business to Business (B2B) payment problem.  Communication and responsiveness is key; without these, invoices often go unpaid and relationships become strained.  Bizni aims to eliminate these problems through an all in one style platform where businesses small and large can come together in an open, honest, and easy environment.</p>
+
+					
+						<h3>Our team</h3>
+					<h5>Dan Stern</h5> <p>Dan is currently in college studying business.</p>
+					<h5>Joshua Zeitlinger</h5> <p>Joshua is a rising senior at Regis High School with an extensive background on technology an programming.</p>
+					<h5>Ariel Camilo</h5> <p>Ariel graduated Regis High School this past spring and will be attending Marist in the fall of this year.  He has an extensive background in technology and database management.</p>
 					
 					<a href="#" class="image fit"><img src="images/gct.jpg" alt="GCT Workspace" /></a><!-- cool image -->
 					<div class="center" style="text-align:center;">The Grand Central Tech Co-Working Space</div>
+				
 				</div>
 			</section>
 		<!-- include the footer via php -->
