b1185e87e670e97148fa74932e6a6b203af55ed0
Joshua Zeitlinger <joshuaspring9@gmail.com>
2015-07-20 17:16:48 -0400

    Change about us description

--- about.php
+++ about.php
@@ -19,10 +19,11 @@
 				<div class="container">
 					<header class="major special">
 						<h2>Who are we?</h2>
-						<p>We are a group of interns at the <a target="_blank" href="http://grandcentraltech.com/">Grand Central Tech Innovation</a> project.  Bizni was created in response to the challenge of the Business to Business (B2B) payment problem, where invoices often go unpaid and relationships are strained.  Bizni aims to solve these problems through an all in one style quality product.</p>
+						<p>Bizni was created by a group of interns at the <a target="_blank" href="http://grandcentraltech.com/">Grand Central Tech Innovation</a> project as a response to the challenge of the Business to Business (B2B) payment problem.  Communication and responsiveness is key; without these, invoices often go unpaid and relationships become strained.  Bizni aims to eliminate these problems through an all in one style platform where businesses small and large can come together in an open, honest, and easy environment.</p>
 					</header>
 					
 					<a href="#" class="image fit"><img src="images/gct.jpg" alt="GCT Workspace" /></a><!-- cool image -->
+					<div class="center" style="text-align:center;">The Grand Central Tech Co-Working Space</div>
 				</div>
 			</section>
 		<!-- include the footer via php -->
