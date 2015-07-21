0e2f21d19d39eb46630f72c21314184efcbdd140
Ariel Camilo <arielcamilo97@gmail.com>
2015-07-20 22:53:07 -0400

    Update Ariel's Bio

--- about.php
+++ about.php
@@ -24,15 +24,15 @@
 					<h3>Background</h3>
 					<p>Bizni was created by a group of interns at the <a target="_blank" href="http://grandcentraltech.com/">Grand Central Tech Innovation</a> project as a response to the challenge of the Business to Business (B2B) payment problem.  Communication and responsiveness is key; without these, invoices often go unpaid and relationships become strained.  Bizni aims to eliminate these problems through an all in one style platform where businesses small and large can come together in an open, honest, and easy environment.</p>
 
-					
+
 						<h3>Our team</h3>
 					<h5>Dan Stern</h5> <p>Dan is currently in college studying business.</p>
 					<h5>Joshua Zeitlinger</h5> <p>Joshua is a rising senior at Regis High School with an extensive background in technology and programming.  He brings his experience of maintaining the website <a href="201weather.com">201weather.com</a> as well as several other projects involving Google Maps and 3D Printing APIs.  He is also experienced in both front end (Javascript) and backend (PHP) web scripting.</p>
-					<h5>Ariel Camilo</h5> <p>Ariel graduated Regis High School this past spring and will be attending Marist in the fall of this year.  He has an extensive background in technology and database management.</p>
-					
+					<h5>Ariel Camilo</h5> <p>Ariel Camilo is a recent Regis High School graduate, who will be studying Computer Science at Marist College in the Fall.  His expierence falls in the web development category, and in database management.</p>
+
 					<a href="#" class="image fit"><img src="images/gct.jpg" alt="GCT Workspace" /></a><!-- cool image -->
 					<div class="center" style="text-align:center;">The Grand Central Tech Co-Working Space</div>
-				
+
 				</div>
 			</section>
 		<!-- include the footer via php -->
